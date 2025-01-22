35fe99f3191e530f7bba25a2b36a38dea79d2c80
Jasper Boeijenga <jboeijenga@gmail.com>
2025-01-22 17:11:19 +0100

    Added support for registering icon-packs (#59)
    Signed-off-by: Jasper Boeijenga <jboeijenga@gmail.com>

--- src/Mermaid/props.ts
+++ src/Mermaid/props.ts
@@ -1,7 +1,9 @@
 import { MermaidConfig } from 'mermaid';
+import { IconLoader } from 'mermaid/dist/rendering-util/icons';
 
 export type MermaidProps = {
     lightConfig?: MermaidConfig,
     darkConfig?: MermaidConfig,
     config?: MermaidConfig,
+    iconLoaders?: IconLoader[]
 }
